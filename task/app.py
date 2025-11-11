724649363ee4d8e65c146d6fa8222d058cb8668a
Aliaksei Liulkovich <aliaksei_liulkovich@epam.com>
2025-11-11 16:40:24 +0100

    Remove TODO comments

--- task/app.py
+++ task/app.py
@@ -9,23 +9,6 @@ from task.models.role import Role
 
 
 async def start(stream: bool) -> None:
-    #TODO:
-    # 1.1. Create DialClient
-    # (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
-    #  you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
-    #  don't forget to add your API_KEY)
-    # 1.2. Create CustomDialClient
-    # 2. Create Conversation object
-    # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
-    #    messages.
-    # 4. Use infinite cycle (while True) and get yser message from console
-    # 5. If user message is `exit` then stop the loop
-    # 6. Add user message to conversation history (role 'user')
-    # 7. If `stream` param is true -> call DialClient#stream_completion()
-    #    else -> call DialClient#get_completion()
-    # 8. Add generated message to history
-    # 9. Test it with DialClient and CustomDialClient
-    # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
 
     dial_client = DialClient(deployment_name = "gpt-5-mini-2025-08-07")
 
--- task/clients/client.py
+++ task/clients/client.py
@@ -10,10 +10,6 @@ class DialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
-        #TODO:
-        # Documentation: https://pypi.org/project/aidial-client/ (here you can find how to create and use these clients)
-        # 1. Create Dial client
-        # 2. Create AsyncDial client
         self._client = Dial(
             api_key=self._api_key,
             base_url=DIAL_ENDPOINT
@@ -24,11 +20,6 @@ class DialClient(BaseClient):
         )
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # 1. Create chat completions with client
-        #    Hint: to unpack messages you can use the `to_dict()` method from Message object
-        # 2. Get content from response, print it and return message with assistant role and content
-        # 3. If choices are not present then raise Exception("No choices in response found")
         response = self._client.chat.completions.create(
             deployment_name=self._deployment_name,
             messages=[msg.to_dict() for msg in messages]
@@ -39,14 +30,6 @@ class DialClient(BaseClient):
         return Message(role=Role.AI, content=content)
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # 1. Create chat completions with async client
-        #    Hint: don't forget to add `stream=True` in call.
-        # 2. Create array with `contents` name (here we will collect all content chunks)
-        # 3. Make async loop from `chunks` (from 1st step)
-        # 4. Print content chunk and collect it contents array
-        # 5. Print empty row `print()` (it will represent the end of streaming and in console we will print input from a new line)
-        # 6. Return Message with assistant role and message collected content
         chunks = await self._async_client.chat.completions.create(
             deployment_name=self._deployment_name,
             messages=[msg.to_dict() for msg in messages],
